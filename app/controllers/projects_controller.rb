ecec7c8c40980db3e4117bc18f9f40c189cabb9d
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-09 19:49:06 +0500

    add user actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new project_params
+    @project.user_id = current_user.id
     if @project.save
       redirect_to projects_path, notice: 'Project in successfully create'
     else
